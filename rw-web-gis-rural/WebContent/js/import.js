dd7e882469fe19a40d955c933e1213bb9a81431b
jin_igarashi <jin_igarashi@kk-grp.jp>
2018-03-08 13:26:50 +0900

    CSS等を整理

---
+++ rw-web-gis-rural/WebContent/js/import.js
@@ -0,0 +1,27 @@
+function importJS() {
+	var jsFiles = [
+        './js/external/jquery-3.3.1.min.js',
+        './js/external/jquery-ui.min.js',
+        './js/external/leaflet/leaflet.js',
+        './js/external/leaflet/leaflet-hash.js',
+        './js/external/leaflet/Leaflet.NavBar.js',
+        './js/external/leaflet/Leaflet.PolylineMeasure.js',
+        './js/external/leaflet/Leaflet.Coordinates-0.1.5.min.js',
+        './js/external/leaflet/leaflet.easyPrint.js',
+        './js/external/leaflet/leaflet.groupedlayercontrol.js',
+        './js/external/leaflet/L.Control.Locate.js',
+        './js/external/leaflet/L.Control.SmallWidget.js',
+        './js/external/leaflet/L.Control.HtmlLegend.js',
+        './js/external/leaflet/Leaflet.GraphicScale.min.js',
+        './js/external/leaflet/easy-button.js',
+        './js/external/leaflet/iconLayers.js',
+        './js/external/leaflet/Control.Geocoder.js',
+        //'./js/gis/lib/gis.js',
+        './js/gis/gis.min.js',
+        './js/app.js',
+        ];
+	var scriptTags = [];
+	for (var i=0; i<jsFiles.length; i++) {scriptTags.push("<script src='" + jsFiles[i] + "'><\/script>");}
+	if (scriptTags.length > 0) {document.write(scriptTags.join(""));}
+}
+importJS();
